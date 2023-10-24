fa135da98fcdcc42fab1cd1c810b207dc30839a9
Vadim_Zubenko <VZworkmail@yandex.ru>
2023-10-25 00:27:25 +0700

    hw_3

---
+++ hw_3/SRP/ClalucateSalary.java
@@ -0,0 +1,15 @@
+package SRP;
+
+public class CalculateSalary {
+    int baseSalary;
+    int valueTime;
+
+    public CalculateSalary(int baseSalary, int valueTime) {
+        this.baseSalary = baseSalary;
+        this.valueTime = valueTime;
+    }
+    public int calculateSalary(){
+        return valueTime * baseSalary;
+    }
+
+}
